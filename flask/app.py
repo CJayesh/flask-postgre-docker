83c038e7e98c21d665c914402aa36c069aa5d62f
cJayesh <chavanjayesh63@gmail.com>
2021-04-25 02:15:40 +0530

    database connection fixes

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config.from_object("config.Config")
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Worlds!'
+    return '''<h1>User Reviews</h1>'''
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",
